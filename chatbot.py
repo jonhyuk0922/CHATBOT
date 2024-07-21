ae58c62d25857ca084b83b7dab9aa951855fc5ba
jonhyuk0922 <jonhyuk0922@naver.com>
2024-07-22 00:31:34 +0900

    🎨Ref/ chatbot.py add doc string

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,6 @@ from utils import load_model, set_memory, initialize_chain, generate_message
 st.title("페르소나 챗봇")
 st.markdown("<br>", unsafe_allow_html=True)  # 브라우저에 줄 바꿈을 삽입합니다.
 
-<<<<<<< HEAD
 # 사용자로부터 캐릭터를 선택받기 위한 드롭다운 메뉴 설정
 character_name = st.selectbox(
     "**캐릭터를 골라줘!**",
@@ -33,20 +32,6 @@ model_name = st.selectbox(
     key="model_name_select",
 )
 # 선택된 모델 이름을 세션 상태에 저장
-=======
-character_name = st.selectbox("**캐릭터를 골라줘!**", 
-                              ("baby_shark", "one_zero"), 
-                              index=0, 
-                              key="character_name_select")
-
-st.session_state.character_name = character_name
-
-model_name = st.selectbox("**모델을 골라줘!**", 
-                          ("gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"), 
-                          index=0, 
-                          key="model_name_select")
-
->>>>>>> parent of dfc07c7 (🎨Ref/black formatting chatbot.py)
 st.session_state.model_name = model_name
 
 # 세션 상태에서 채팅 시작 여부를 확인 및 초기화
@@ -55,10 +40,15 @@ if "chat_started" not in st.session_state:
     st.session_state.memory = None
     st.session_state.chain = None
 
-<<<<<<< HEAD
 
 # 채팅을 시작하는 함수 정의
-def start_chat():
+def start_chat() -> None:
+    """
+    선택된 모델과 캐릭터를 기반으로 채팅을 시작합니다.
+
+    Streamlit 세션 상태를 사용하여 사용자가 선택한 모델과 캐릭터 이름에 따라
+    언어 모델을 로드하고 대화 메모리를 설정하며, LLM 체인을 초기화합니다.
+    """
     llm = load_model(st.session_state.model_name)  # 선택된 모델을 로드합니다.
     st.session_state.chat_started = True  # 채팅 시작 상태를 True로 설정합니다.
     st.session_state.memory = set_memory()  # 메모리를 초기화합니다.
@@ -66,16 +56,6 @@ def start_chat():
         llm, st.session_state.character_name, st.session_state.memory
     )  # 체인을 초기화합니다.
 
-=======
-def start_chat() -> None:
-    """
-    선택된 모델과 캐릭터를 기반으로 채팅을 시작합니다.
-    """
-    llm = load_model(st.session_state.model_name)
-    st.session_state.chat_started = True
-    st.session_state.memory = set_memory()
-    st.session_state.chain = initialize_chain(llm, st.session_state.character_name, st.session_state.memory)
->>>>>>> parent of dfc07c7 (🎨Ref/black formatting chatbot.py)
 
 # "Start Chat" 버튼을 클릭했을 때 start_chat 함수를 호출
 if st.button("Start Chat"):
@@ -114,94 +94,6 @@ if st.session_state.chat_started:
             # 응답을 단어 단위로 나누어 점진적으로 화면에 표시
             for chunk in response_content.split():
                 full_response += chunk + " "
-<<<<<<< HEAD
                 time.sleep(0.05)  # 각 단어 사이에 지연을 추가하여 애니메이션 효과
                 message_placeholder.markdown(full_response + "▌")  # 진행 중 표시
             message_placeholder.markdown(full_response.strip())  # 최종 응답 표시
-=======
-                time.sleep(0.05)
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response.strip())
-
-
-def load_model(model_name: str) -> ChatOpenAI:
-    """
-    주어진 모델 이름을 기반으로 ChatOpenAI 모델을 로드합니다.
-
-    Args:
-        model_name (str): 사용할 모델의 이름.
-
-    Returns:
-        ChatOpenAI: 로드된 ChatOpenAI 모델.
-    """
-    load_dotenv()
-    
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name=model_name)
-    return llm 
-
-
-def load_prompt(character_name: str) -> str:
-    """
-    캐릭터 이름에 따라 프롬프트 파일을 로드합니다.
-
-    Args:
-        character_name (str): 캐릭터의 이름.
-
-    Returns:
-        str: 로드된 프롬프트 내용.
-    """
-    with open(f"prompts/{character_name}.prompt", "r", encoding="utf-8") as file:
-        prompt = file.read().strip()
-    return prompt
-
-
-def set_memory() -> ConversationBufferMemory:
-    """
-    대화 히스토리를 저장하기 위한 메모리를 설정합니다.
-
-    Returns:
-        ConversationBufferMemory: 초기화된 대화 메모리.
-    """
-    return ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-
-def initialize_chain(llm: ChatOpenAI, character_name: str, memory: ConversationBufferMemory) -> LLMChain:
-    """
-    주어진 LLM과 캐릭터 이름, 메모리를 기반으로 체인을 초기화합니다.
-
-    Args:
-        llm (ChatOpenAI): 사용할 언어 모델.
-        character_name (str): 캐릭터의 이름.
-        memory (ConversationBufferMemory): 대화 메모리.
-
-    Returns:
-        LLMChain: 초기화된 LLM 체인.
-    """
-    system_prompt = load_prompt(character_name)
-    custom_prompt = ChatPromptTemplate(
-        messages=[
-            SystemMessagePromptTemplate.from_template(system_prompt),
-            MessagesPlaceholder(variable_name="chat_history"),
-            HumanMessagePromptTemplate.from_template("{input}"),
-        ]
-    )
-    chain = LLMChain(llm=llm, prompt=custom_prompt, verbose=True, memory=memory)
-    return chain
-
-
-def generate_message(chain: LLMChain, user_input: str) -> str:
-    """
-    사용자 입력을 기반으로 메시지를 생성합니다.
-
-    Args:
-        chain (LLMChain): 사용할 체인.
-        user_input (str): 사용자의 입력.
-
-    Returns:
-        str: 생성된 응답 메시지.
-    """
-    result = chain({"input": user_input})
-    response_content = result["text"]
-    return response_content
->>>>>>> parent of dfc07c7 (🎨Ref/black formatting chatbot.py)
