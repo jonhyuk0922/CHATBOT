d97a51e48b935bf6e808b96cd8b92c6e6cc009ae
jonhyuk0922 <jonhyuk0922@naver.com>
2024-07-22 00:27:30 +0900

    fix

--- utils.py
+++ utils.py
@@ -4,67 +4,124 @@
 # 해당 리포지토리의 라이센스에 따라 사용되었습니다.
 # -------------------------------------------------------------------------
 
-import os
+import streamlit as st
+import time
+from typing import Optional
+from langchain_core.messages import HumanMessage, AIMessage
+from utils import load_model, set_memory, initialize_chain, generate_message
 from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
-from dotenv import load_dotenv
+
+st.title("페르소나 챗봇")
+st.markdown("<br>", unsafe_allow_html=True)
+
+character_name = st.selectbox("**캐릭터를 골라줘!**", 
+                              ("baby_shark", "one_zero"), 
+                              index=0, 
+                              key="character_name_select")
+
+st.session_state.character_name = character_name
+
+model_name = st.selectbox("**모델을 골라줘!**", 
+                          ("gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"), 
+                          index=0, 
+                          key="model_name_select")
+
+st.session_state.model_name = model_name
+
+if "chat_started" not in st.session_state:
+    st.session_state.chat_started = False
+    st.session_state.memory = None
+    st.session_state.chain = None
+
+def start_chat() -> None:
+    """
+    선택된 모델과 캐릭터를 기반으로 채팅을 시작합니다.
+    """
+    llm = load_model(st.session_state.model_name)
+    st.session_state.chat_started = True
+    st.session_state.memory = set_memory()
+    st.session_state.chain = initialize_chain(llm, st.session_state.character_name, st.session_state.memory)
+
+if st.button("Start Chat"):
+    start_chat()
+
+st.markdown("<br>", unsafe_allow_html=True)
+
+if st.session_state.chat_started:
+    if st.session_state.memory is None or st.session_state.chain is None:
+        start_chat()
+
+    for message in st.session_state.memory.chat_memory.messages:
+        if isinstance(message, HumanMessage):
+            role = "user"
+        elif isinstance(message, AIMessage):
+            role = "assistant"
+        else:
+            continue
+        with st.chat_message(role):
+            st.markdown(message.content)
+
+    if prompt := st.chat_input():
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            response_content = generate_message(st.session_state.chain, prompt)
+
+            for chunk in response_content.split():
+                full_response += chunk + " "
+                time.sleep(0.05)
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response.strip())
 
 
-# 환경 변수에서 모델 로드 함수
 def load_model(model_name: str) -> ChatOpenAI:
-    """dotenv 로부터 key 저장하고, model_name 으로 모델 버전 받아와서 LLM 할당하여 반환합니다.
+    """
+    주어진 모델 이름을 기반으로 ChatOpenAI 모델을 로드합니다.
 
     Args:
-        model_name (str): streamlit 에서 옵션으로 받아오는 모델 버전.
+        model_name (str): 사용할 모델의 이름.
 
     Returns:
-        ChatOpenAI: llm 모델 반환합니다.
+        ChatOpenAI: 로드된 ChatOpenAI 모델.
     """
     load_dotenv()
-
+    
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name=model_name)
-    return llm
+    return llm 
 
 
-# 캐릭터 이름에 따라 프롬프트 파일 로드 함수
 def load_prompt(character_name: str) -> str:
-    """받아온 캐릭터에 대한 사전정의 prompt 를 읽어옵니다.
+    """
+    캐릭터 이름에 따라 프롬프트 파일을 로드합니다.
 
     Args:
-        character_name (str): streamlit 에서 옵션으로 받는 캐릭터명.
+        character_name (str): 캐릭터의 이름.
 
     Returns:
-        str: 사전에 정의된 프롬프트 내용.
+        str: 로드된 프롬프트 내용.
     """
     with open(f"prompts/{character_name}.prompt", "r", encoding="utf-8") as file:
-        prompt = file.read().strip()  # 프롬프트 파일을 읽어옵니다.
+        prompt = file.read().strip()
     return prompt
 
 
-# 메모리 설정 함수
 def set_memory() -> ConversationBufferMemory:
-    """ConversationBufferMemory 객체를 생성하고 반환합니다.
+    """
+    대화 히스토리를 저장하기 위한 메모리를 설정합니다.
 
     Returns:
-        ConversationBufferMemory: 메모리 키 'chat_history'로 설정되고 메시지를 반환하도록 구성된 ConversationBufferMemory 인스턴스.
+        ConversationBufferMemory: 초기화된 대화 메모리.
     """
-    return ConversationBufferMemory(
-        memory_key="chat_history", return_messages=True
-    )  # 대화 히스토리를 저장하는 메모리를 초기화합니다.
+    return ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 
-# 체인 초기화 함수
-def initialize_chain(
-    llm: ChatOpenAI, character_name: str, memory: ConversationBufferMemory
-) -> LLMChain:
+def initialize_chain(llm: ChatOpenAI, character_name: str, memory: ConversationBufferMemory) -> LLMChain:
     """
     주어진 LLM과 캐릭터 이름, 메모리를 기반으로 체인을 초기화합니다.
 
@@ -76,27 +133,18 @@ def initialize_chain(
     Returns:
         LLMChain: 초기화된 LLM 체인.
     """
-    system_prompt = load_prompt(character_name)  # 시스템 프롬프트를 로드합니다.
+    system_prompt = load_prompt(character_name)
     custom_prompt = ChatPromptTemplate(
         messages=[
-            SystemMessagePromptTemplate.from_template(
-                system_prompt
-            ),  # 시스템 메시지 프롬프트를 설정합니다.
-            MessagesPlaceholder(
-                variable_name="chat_history"
-            ),  # 대화 히스토리를 위한 플레이스홀더를 설정합니다.
-            HumanMessagePromptTemplate.from_template(
-                "{input}"
-            ),  # 사용자 입력을 위한 프롬프트를 설정합니다.
+            SystemMessagePromptTemplate.from_template(system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{input}"),
         ]
     )
-    chain = LLMChain(
-        llm=llm, prompt=custom_prompt, verbose=True, memory=memory
-    )  # LLM 체인을 초기화합니다.
+    chain = LLMChain(llm=llm, prompt=custom_prompt, verbose=True, memory=memory)
     return chain
 
 
-# 메세지 생성 함수
 def generate_message(chain: LLMChain, user_input: str) -> str:
     """
     사용자 입력을 기반으로 메시지를 생성합니다.
@@ -108,6 +156,6 @@ def generate_message(chain: LLMChain, user_input: str) -> str:
     Returns:
         str: 생성된 응답 메시지.
     """
-    result = chain({"input": user_input})  # 사용자 입력을 기반으로 결과를 생성합니다.
-    response_content = result["text"]  # 결과에서 텍스트를 추출합니다.
+    result = chain({"input": user_input})
+    response_content = result["text"]
     return response_content
